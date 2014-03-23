85c76baf5c13cfe67af2c56af719b392b068ff31
Benoit Sautel <benoit.sautel@gmail.com>
2014-03-23 09:07:11 +0100

    Add a tasks tab

--- app.js
+++ app.js
@@ -6,9 +6,32 @@ function HelloController($scope) {
 
 function EventsController($scope) {
     $scope.clicksCount = 0;
-    $scope.click = function() {
+    $scope.click = function click() {
         this.clicksCount++;
     }
+}
+
+function TasksController($scope) {
+    $scope.tasks = [{label: 'Say hello', done: false}];
+    $scope.createNewTask = function createNewTask() {
+        $scope.newTask = {label: '', done: false};
+    };
+    $scope.saveNewTask = function saveNewTask() {
+        $scope.tasks.push($scope.newTask);
+        delete $scope.newTask;
+    };
+    $scope.cancelTaskCreation = function cancelTaskCreation() {
+        delete $scope.newTask;
+    };
+    $scope.isNewTaskValid = function isNewTaskValid() {
+        return $scope.newTask &&  $scope.newTask.label.trim().length > 0;
+    };
+    $scope.deleteTask = function deleteTask(task) {
+        var index = $scope.tasks.indexOf(task);
+        if (index > -1) {
+            $scope.tasks.splice(index, 1);
+        }
+    };
 }
 
 angular.module('test', []).config(
@@ -18,15 +41,19 @@ angular.module('test', []).config(
             $routeProvider.
                 when('/hello', {templateUrl: 'hello.html', controller: HelloController}).
                 when('/events', {templateUrl: 'events.html', controller: EventsController}).
+                when('/tasks', {templateUrl: 'tasks.html', controller: TasksController}).
                 otherwise({redirectTo: '/hello'});
             }
     ]);
 
 function NavBarController($scope, $location) {
-    $scope.isHello = function() {
+    $scope.isHello = function isHello() {
         return $location.path() == '/hello';
     };
-    $scope.isEvents = function() {
+    $scope.isEvents = function isEvents() {
         return $location.path() == '/events';
+    };
+    $scope.isTasks = function isTasks() {
+        return $location.path() == '/tasks';
     };
 }
